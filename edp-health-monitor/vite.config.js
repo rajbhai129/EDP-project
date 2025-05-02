e47990ab62da84e0e0fab890408438a10fbbe8ce
rajbhai129 <hemrajlalawat52@gmail.com>
2025-05-03 01:22:17 +0530

    minor change

--- edp-health-monitor/vite.config.js
+++ edp-health-monitor/vite.config.js
@@ -1,17 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
       manifest: {
-        name: "SmartCare",
-        short_name: "SmartCare",
+        name: "SmartHealthCare",
+        short_name: "SmartHealthCare",
         description: "Real-time Health Monitoring App",
         theme_color: "#ffffff",
         background_color: "#ffffff",
